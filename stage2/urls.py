7e4c80633a2a9bdefb01db221725d9d1bfdef001
Kingsley <kinabonyi@gmail.com>
2023-09-15 11:44:44 +0100

    url settings

--- stage2/urls.py
+++ stage2/urls.py
@@ -3,6 +3,6 @@ from .views import add, update
 
 
 urlpatterns = [
-    path('add/', add, name='add'),
-    path('update/<int:pk>/', update, name='update')
+    path('', add, name='add'),
+    path('user_id/<int:pk>/', update, name='update')
 ]
