94705ca13ae2862aff729ac74baec88a4465ef8f
Kingsley <kinabonyi@gmail.com>
2023-09-17 21:59:25 +0100

    added the url

--- stage2/urls.py
+++ stage2/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ListCreatePerson, RetrieveUpdateDelete
+from .views import ListCreatePerson, RetrieveUpdateDeletePerson
 
 
 urlpatterns = [
     path('', ListCreatePerson.as_view()),
-    path('<int:pk>', RetrieveUpdateDelete.as_view())
+    path('<int:pk>', RetrieveUpdateDeletePerson.as_view())
 ]
