08fff6e2c386296c1ed187d9a2f8876e2b3e6a48
Kingsley <kinabonyi@gmail.com>
2023-09-17 13:52:16 +0100

    url setting

--- stage2/urls.py
+++ stage2/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import add, update
+from .views import add, update, get_person
 
 
 urlpatterns = [
     path('', add, name='add'),
-    path('/<int:pk>/', update, name='update')
+    path('/<int:pk>/', update, name='update'),
+    path('get_person/<int:pk>/', name='get_person')
 ]
